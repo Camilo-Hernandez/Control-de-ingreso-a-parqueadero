5f55cfd9b7238dbdb9faf24aac92335f690862d8
CapitanLevi <36543483+CapitanLevi@users.noreply.github.com>
2020-05-23 18:00:45 -0500

    Falta: palanqueras permanezcan arriba y vuelvan a bajar
    ...luego de un tiempo
    "En el caso del carril de salida, cuando se activa el sensor que se encuentra al interior
    del parqueadero, la talanquera debe abrirse por un periodo de máximo un minuto o
    hasta que se detecte la salida del vehículo, es decir, que el sensor exterior se active y
    posteriormente se desactive; con respecto al carril de entrada, sucede algo similar,
    solo que en esta ocasión, el sensor que se encarga de abrir la
    talanquera es el ubicado en el exterior del parqueadero."

--- codigo.cpp
+++ codigo.cpp
@@ -10,10 +10,10 @@
   para simular los switches como sensores IR y
   comprobar si un auto que pasa en medio de los
   sensores IR entra o sale.
-  
+
   LED:
   Los LED rojos muestran el estado de cada switch (1 o 0)
-  
+
   Secuencias y Estados de los switches y del sistema:
   Entrar está representado por
   la secuencia de estados 10, 11, 01, 00.
@@ -42,7 +42,7 @@
   secuencia[1] = secuencia[2]
   secuencia[2] = secuencia[3]
   secuencia[3] = estado
-  
+
   El circuito:
   * Los cables negros indican conexión a GND.
   * Los cables rojos indican conexión a +5V.
@@ -63,134 +63,138 @@
 // Servomotores (palanquera a la entrada y a la salida)
 Servo servo1;
 Servo servo2;
+// Pines de los switches y leds, estados actuales y anteriores
+// de los switches para detectar el cambio de estado
 // Switch 1
-int switch1Pin = 12;		// Pin Switch 1
-int led1Pin = 11;			// Pin LED 1
-int switch1State = 0;		// Estado actual del Switch 1
-int lastSwitch1State = 0;	// Estado anterior de S1
-int bit1 = B0;				// Valor bit1 del estado
+int pinSwitch1 = 12;	
+int led1 = 11;
+int switch1 = 0;
+int lastSwitch1 = 0;
 // Switch 2
-int switch2Pin = 9;			// Pin Switch 2
-int led2Pin = 8;			// Pin LED 2
-int switch2State = 0;		// Estado actual del Switch 2
-int lastSwitch2State = 0;	// Estado anterior de S2
-int bit2 = B0;				// Valor bit2 del estado
+int pinSwitch2 = 9;
+int led2 = 8;
+int switch2 = 0;
+int lastSwitch2 = 0;
 // Switch 3
-int switch3Pin = 9;			// Pin Switch 3
-int led3Pin = 8;			// Pin LED 3
-int switch3State = 0;		// Estado actual del Switch 3
-int lastSwitch3State = 0;	// Estado anterior de S3
-int bit3 = B0;				// Valor bit3 del estado
+int pinSwitch3 = 6;
+int led3 = 7;
+int switch3 = 0;
+int lastSwitch3 = 0;
 // Switch 4
-int switch4Pin = 9;			// Pin Switch 4
-int led4Pin = 8;			// Pin LED 4
-int switch4State = 0;		// Estado actual del Switch 4
-int lastSwitch4State = 0;	// Estado anterior de S4
-int bit4 = B0;				// Valor bit4 del estado
-// Con ambos bits = 0, el estado inicial es 00.
-// O sea, estamos en el estado en que ningún carro se ha asomado
-int estado = (bit1<<1)+bit2;
-// Y con el primer estado configurado, la secuencia es:
-int secuencia[] = {0,0,0,estado};
-// La secuencia se comparará con las de entrada y salida:
-int secuenciaEntrada[] = {B10,B11,B01,B00};
-int secuenciaSalida[] = {B01,B11,B10,B00};
+int pinSwitch4 = 3;
+int led4 = 4;
+int switch4 = 0;
+int lastSwitch4 = 0;
+// Estado inicial es 00.
+// O sea, estamos en el estado en que ningún carro ha pasado
+// por ningún acceso.
+int estado = 0;
+// La secuencia de referencia se comparará con las de entrada y salida:
+int secuenciaEntrada[] = {0,0,0,0};
+int secuenciaSalida[] = {0,0,0,0};
+int secuenciaReferencia[] = {B10,B11,B01,B00};
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(switch1Pin, INPUT);	// Pin de switch 1 como entrada
-  pinMode(switch2Pin, INPUT);	// Pin de switch 2 como entrada
-  pinMode(led1Pin, OUTPUT);		// Pin de led 1 como salida
-  pinMode(led2Pin, OUTPUT);		// Pin de led 2 como salida
-  servo1.attach(10);			// Pin Servo 1
-  //rvo2.attach(10);			// Pin Servo 2
-  servo1.write(5);				
-  /*
-  Serial.println("Secuencia de Entrada:");
-  Serial.println(secuenciaEntrada[0],BIN);
-  Serial.println(secuenciaEntrada[1],BIN);
-  Serial.println(secuenciaEntrada[2],BIN);
-  Serial.println(secuenciaEntrada[3],BIN);
-  */
+  // Pines de switches como entrada
+  pinMode(pinSwitch1, INPUT);
+  pinMode(pinSwitch2, INPUT);
+  pinMode(pinSwitch3, INPUT);
+  pinMode(pinSwitch4, INPUT);
+  // Pines de LEDs como salida
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  // Pines de los servos y ángulos iniciales
+  servo1.attach(10);
+  servo2.attach(5);
+  servo1.write(0);
+  servo2.write(0);
   }
 
 void loop()
 {
-  // Leer el estado actual de los Switches o pines.
-  switch1State = digitalRead(switch1Pin);
-  switch2State = digitalRead(switch2Pin);
+  // Leer el estado actual de los Switches.
+  switch1 = digitalRead(pinSwitch1);
+  switch2 = digitalRead(pinSwitch2);
+  switch3 = digitalRead(pinSwitch3);
+  switch4 = digitalRead(pinSwitch4);
+  // LEDs ON/OFF según el valor de los switches.
+  digitalWrite(led1, switch1);
+  digitalWrite(led2, switch2);
+  digitalWrite(led3, switch3);
+  digitalWrite(led4, switch4);
   
   // Comparar los estados actuales de los switches con los anteriores.
   // Si son diferentes, hay cambio de estado (detección de flanco).
-  if ((switch1State != lastSwitch1State) || (switch2State != lastSwitch2State)) {
-    if (switch1State == HIGH) {
-      // Si el estado actual del switch1 es HIGH,
-      // el switch pasó de 0V a +5V.
-      bit1 = B1;
-    } else {
-      // Si el estado actual está en OFF, entonces
-      // el switch pasó de ON a OFF.
-      bit1 = B0;
-    }
-    if (switch2State == HIGH) {
-      // Si el estado actual del switch2 es HIGH,
-      // el switch pasó de 0V a +5V.
-      bit2 = B1;
-    } else {
-      // Si el estado actual está en OFF, entonces
-      // el switch pasó de ON a OFF.
-      bit2 = B0;
-    }
-    // 'estado' es la concatenación de los bits.
-    // Es decir, estado del sistema es la unión de los estados de los switches.
-    estado = (bit1<<1)+bit2;
-    // Imprimir en binario para ver los estados.
-    //Serial.println(estado,BIN);
-    // Poner un pequeño delay para evitar rebote.
+  if ((switch1 != lastSwitch1) || (switch2 != lastSwitch2)) {
+    // Poner un pequeño delay para evitar rebote por cambio de estado.
     delay(5);
-    // Guarda el 'estado' en el historial de estados, o sea, en 'secuencia'
-    secuencia[0] = secuencia[1];
-    secuencia[1] = secuencia[2];
-    secuencia[2] = secuencia[3];
-    secuencia[3] = estado;
-/*    // Imprimir la secuencia
-	Serial.println("Secuencia:");
-    Serial.println(secuencia[0],BIN);
-    Serial.println(secuencia[1],BIN);
-    Serial.println(secuencia[2],BIN);
-    Serial.println(secuencia[3],BIN);
-*/
-    if(array_cmp(secuencia,secuenciaEntrada)){
-      Serial.println("ENTRADA EXITOSA");
-      for (int pos = 0; pos <= 90; pos += 1) {
-        // tell servo to go to position in variable 'pos'
-        servo1.write(pos);
-        // wait 15 ms for servo to reach the position
-        delay(15); // Wait for 15 millisecond(s)
-      }
+    estado = (switch1<<1)|switch2;
+    // Imprimir en binario para ver los estados.
+    Serial.println(estado,BIN);
+    actualizarSecuencia(estado,secuenciaEntrada);
+  } else if ((switch3 != lastSwitch3) || (switch4 != lastSwitch4)) {
+  	// Poner un pequeño delay para evitar rebote por cambio de estado
+    delay(5);
+    estado = (switch3<<1)|switch4;
+    // Imprimir en binario para ver los estados.
+    Serial.println(estado,BIN);
+    actualizarSecuencia(estado,secuenciaSalida);
+  }
+  
+  if(array_cmp(secuenciaEntrada,secuenciaReferencia)){
+    Serial.println("ENTRADA EXITOSA");
+    for (int pos = 0; pos <= 90; pos += 1) {
+      // tell servo to go to position in variable 'pos'
+      servo1.write(pos);
+      // wait 15 ms for servo to reach the position
+      delay(15); // Wait for 15 millisecond(s)
     }
-    else if(array_cmp(secuencia,secuenciaSalida)){
-      Serial.println("SALIDA EXITOSA");
-      for (int pos = 90; pos >= 0; pos -= 1) {
-        // tell servo to go to position in variable 'pos'
-        servo1.write(pos);
-        // wait 15 ms for servo to reach the position
-        delay(15); // Wait for 15 millisecond(s)
-      }
+    secuenciaEntrada[3] = 5;
+  }
+  else if(array_cmp(secuenciaSalida,secuenciaReferencia)){
+    Serial.println("SALIDA EXITOSA");
+    for (int pos = 0; pos <= 90; pos += 1) {
+      // tell servo to go to position in variable 'pos'
+      servo2.write(pos);
+      // wait 15 ms for servo to reach the position
+      delay(15); // Wait for 15 millisecond(s)
     }
+    secuenciaSalida[3] = 5;
   }
   // Guarda la lectura actual de los pines como la anterior,
   // para la próxima iteración del ciclo.
-  lastSwitch1State = switch1State;
-  lastSwitch2State = switch2State;
-  // LEDs ON/OFF según el valor de los switches.
-  digitalWrite(led1Pin, bit1);
-  digitalWrite(led2Pin, bit2);
+  lastSwitch1 = switch1;
+  lastSwitch2 = switch2;
+  lastSwitch3 = switch3;
+  lastSwitch4 = switch4;
+  
 }
 
 // FUNCIONES
+
 boolean array_cmp(int *a, int *b){
+  // Devuelve true si dos arreglos de igual longitud son iguales.
   for (int n=0;n<4;n++){if (a[n]!=b[n]){return false;}}
-  return true;  
+  return true;
+}
+
+void actualizarSecuencia(int estado,int secuencia[]){
+  // Esta función actualiza la secuencia de entrada y la de salida
+  // Al guardar el 'estado' en la 'secuencia'
+  secuencia[0] = secuencia[1];
+  secuencia[1] = secuencia[2];
+  secuencia[2] = secuencia[3];
+  secuencia[3] = estado;
+  /*
+  // Imprimir la secuencia
+  Serial.println("Secuencia:");
+  Serial.println(secuencia[0],BIN);
+  Serial.println(secuencia[1],BIN);
+  Serial.println(secuencia[2],BIN);
+  Serial.println(secuencia[3],BIN);
+  */
 }
